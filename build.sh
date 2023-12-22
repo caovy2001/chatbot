f85da4101fe72c1d2e648401b8f7cac45d41cbfd
Cao Đinh Sỹ Vỹ <caodinhsyvy@Vys-Mac.local>
2023-12-22 16:15:10 +0000

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
-mvnw clean install
+./mvnw clean install
 cd target
 split -b 21M chatbot-0.0.1-SNAPSHOT.jar splited-app-
 cd ..
 rm build-file/splited-app-*
-mv target/splited-app-* build-file/splited-app-*
+mv target/splited-app-* build-file/
+git add .
+git commit -m 'update'
+git push origin main
+#ssh root@14.225.207.19 'cd chatbot && git pull origin main && docker compose up -d --build && docker system prune -a'
