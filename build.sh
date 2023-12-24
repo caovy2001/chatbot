86612d25cfc935cd48f49ae3b329cd149ecb141b
Cao Đinh Sỹ Vỹ <caodinhsyvy@Vys-Mac.local>
2023-12-24 18:02:20 +0000

    update

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ rm build-file/splited-app-*
 mv target/splited-app-* build-file/
 git add .
 git commit -m 'update'
-git push origin main
-sshpass -p 'Thisispassword123@' ssh root@14.225.207.19 'cd chatbot && git pull origin main && docker compose up -d --build && echo y | docker system prune -a'
+git push origin 'gpt-for-predict'
+sshpass -p 'Thisispassword123@' ssh root@14.225.207.19 'cd chatbot && git checkout gpt-for-predict && git pull origin gpt-for-predict && docker compose up -d --build && echo y | docker system prune -a'
